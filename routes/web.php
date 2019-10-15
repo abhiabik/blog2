cd776f34679fdd9f39e4bcdd003295fb07005eee
abhishek <abhishek.kumar1@crownstack.com>
2019-10-15 15:45:53 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/testSql',function(){
+ $result = DB::insert('insert into users(name,age,gender) values(?,?,?)',['abhishek','26',1]);
+ if($result){
+     return 'Record Created.';
+ }else{
+     return 'Record not created.';
+ }
 });
